eeb08ad2c403714196542698ae9c403f40d58ea2
felipebianchini2006 <kickbuttwovisk@gmail.com>
2026-01-04 21:06:39 -0300

    Mudança para fazer parse seguro para numero evitando erro no Ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const portEnv = process.env['PORT'];
+  const parsedPort = portEnv ? Number(portEnv) : 3000;
+  const port = Number.isNaN(parsedPort) ? 3000 : parsedPort;
+  await app.listen(port);
 }
 bootstrap();
